758fd64e9335e455d952bef7afd6d5caa67f6da8
Michal Masrna <m.masrna1@gmail.com>
2025-04-07 11:15:20 +0200

    sca25519.py introduced introduced SimulationResult class for more readable code

--- sca25519.py
+++ sca25519.py
@@ -5,19 +5,39 @@ from cryptography.hazmat.primitives.asymmetric import x25519
 from cryptography.hazmat.primitives import serialization
 
 
-def parse_output():
-    executable_dir = os.path.dirname(os.path.abspath(__file__))
-    output_dir = os.path.join(executable_dir, "sca25519-ephemeral", "outputs")
+class SimulationResult:
+    address: str
+    hit: int
+    instruction: int
+    result: str
+    output: str
+
+    def __init__(self, address: str, hit: int, instruction: int, result: str, output: str):
+        self.address = address
+        self.hit = hit
+        self.instruction = instruction
+        self.result = result
+        self.output = output
+
+
+def parse_output(output_dir: str) -> Iterable[SimulationResult]:
     output = ""
     for filename in os.listdir(output_dir):
         if filename.endswith(".txt"):
             with open(os.path.join(output_dir, filename), encoding="utf-8") as output_file:
                 output += output_file.read()
 
-    parsed = re.findall(r'#####.+?Address: (0x[a-f0-9]+?)\. Hit: (\d+).+? Run result: (.+?)$.+?Output.+?: ([a-f0-9]+?)$',
+    parsed = re.findall(r'#####.+?Address: (0x[a-f0-9]+?)\. Hit: (\d+).+?Instruction: (\d+).+?Run result: (.+?)$.+?Output.+?: ([a-f0-9]+?)$',
                         output, re.MULTILINE | re.DOTALL)
     for case in parsed:
-        yield case
+        address, hit, instruction, result, output = case
+        yield SimulationResult(
+            address=address,
+            hit=int(hit),
+            instruction=int(instruction),
+            result=result,
+            output=output
+        )
 
 def generate_faulted_results(original_key: bytes) -> Iterable[tuple[bytes, bytes]]:
     fault_masks: list[bytes] = []
@@ -38,24 +58,26 @@ def generate_faulted_results(original_key: bytes) -> Iterable[tuple[bytes, bytes
         yield faulted_key_bytes, public_key_bytes
 
 
-def check_key_shortening():
-    results_from_simulator = list(parse_output())
+def check_key_shortening(output_dir: str):
+    results_from_simulator = list(parse_output(output_dir))
     print(f"Number of fault results: {len(results_from_simulator)}")
     print()
     for faulted_key, result in generate_faulted_results(
         bytes([0x80, 0x65, 0x74, 0xba, 0x61, 0x62, 0xcd, 0x58, 0x49, 0x30, 0x59,
                 0x47, 0x36, 0x16, 0x35, 0xb6, 0xe7, 0x7d, 0x7c, 0x7a, 0x83, 0xde,
                 0x38, 0xc0, 0x80, 0x74, 0xb8, 0xc9, 0x8f, 0xd4, 0x0a, 0x43])):
-        for faulted_address, hit, _, output in results_from_simulator:
-            if output == result.hex():
-                print(f"Skipped address {faulted_address} on hit {hit}.")
+        for result_from_simulator in results_from_simulator:
+            if result_from_simulator.output == result.hex():
+                print(f"Skipped address {result_from_simulator.address} on hit {result_from_simulator.hit}.")
                 print(f"Resulting key - {faulted_key.hex()}.")
-                print(f"Result - {output} ==")
+                print(f"Result - {result_from_simulator.output} ==")
                 print(f"         {result.hex()}.")
 
 
 def main():
-    check_key_shortening()
+    executable_dir = os.path.dirname(os.path.abspath(__file__))
+    original_output_dir = os.path.join(executable_dir, "sca25519-unprotected", "outputs-original")
+    check_key_shortening(original_output_dir)
 
 
 main()
