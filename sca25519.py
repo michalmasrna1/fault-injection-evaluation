fb8e6a3345a3bf52ffe7482e4e15ce2916df66b8
Michal Masrna <m.masrna1@gmail.com>
2025-06-12 12:47:31 +0200

    sca25519 reworked to work with the binary processed outputs

--- sca25519.py
+++ sca25519.py
@@ -1,10 +1,7 @@
 import json
 import os
-import re
-from dataclasses import dataclass
 from itertools import combinations
 from typing import Iterable, Literal
-from unittest import result
 
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import x25519
@@ -13,44 +10,9 @@ from pyecsca.ec.formula import LadderFormula, ScalingFormula
 from pyecsca.ec.mult import LadderMultiplier
 from pyecsca.ec.params import get_params
 from pyecsca.ec.point import Point
+from results import SimulationResult
 
 EXECUTABLE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
-@dataclass
-class SimulationResult:
-    address: str
-    hit: int
-    instruction: int
-    result: str
-    output: str
-
-    def __init__(self, address: str, hit: int, instruction: int, result: str, output: str):
-        self.address = address
-        self.hit = hit
-        self.instruction = instruction
-        self.result = result
-        self.output = output
-
-
-def parse_output(output_dir: str) -> Iterable[SimulationResult]:
-    output = ""
-    for filename in os.listdir(output_dir):
-        if filename.endswith(".txt"):
-            with open(os.path.join(output_dir, filename), encoding="utf-8") as output_file:
-                output += output_file.read()
-
-    parsed = re.findall(r'#####.+?Address: (0x[a-f0-9]+?)\. Hit: (\d+).+?Instruction: (\d+)'
-                        '.+?Run result: (.+?)$.+?Output.+?: ([a-f0-9]+?)$', output, re.MULTILINE | re.DOTALL)
-    for case in parsed:
-        address, hit, instruction, result, output = case
-        yield SimulationResult(
-            address=address,
-            hit=int(hit),
-            instruction=int(instruction),
-            result=result,
-            output=output
-        )
 
 
 def get_public_key_bytes_from_private_bytes(private_bytes: bytes) -> bytes:
@@ -67,6 +29,15 @@ def swap_endian(key: bytes) -> bytes:
     swapped_key = int.from_bytes(key, byteorder='big')
     swapped_key = swapped_key.to_bytes(32, byteorder='little')
     return swapped_key
+
+
+def parse_output(output_dir: str) -> Iterable[SimulationResult]:
+    for filename in os.listdir(output_dir):
+        if filename.endswith(".bin"):
+            with open(os.path.join(output_dir, filename), "rb") as output_file:
+                # Read 64 byte chunks, for each call SimulationResult.from_bytes()
+               while chunk := output_file.read(64):
+                   yield SimulationResult.from_bytes(chunk)
 
 
 def generate_computational_loop_abort_keys():
@@ -195,23 +166,23 @@ def clamp(key: bytes) -> bytes:
 
 
 def check_key_shortening(output_dir: str):
-    results_sim: dict[str, dict[str, set[int]]] = {}
+    results_sim: dict[bytes, dict[bytes, set[int]]] = {}
     for result_sim in parse_output(output_dir):
         if result_sim.output not in results_sim:
             results_sim[result_sim.output] = {}
-        if result_sim.address not in results_sim[result_sim.output]:
-            results_sim[result_sim.output][result_sim.address] = set()
-        results_sim[result_sim.output][result_sim.address].add(result_sim.hit)
+        if result_sim.executed_instruction.address not in results_sim[result_sim.output]:
+            results_sim[result_sim.output][result_sim.executed_instruction.address] = set()
+        results_sim[result_sim.output][result_sim.executed_instruction.address].add(result_sim.executed_instruction.hit)
 
-    seen_effective_keys: dict[bytes, dict[str, set[int]]] = {}
+    seen_effective_keys: dict[bytes, dict[bytes, set[int]]] = {}
     untouched_key = bytes([0x80, 0x65, 0x74, 0xba, 0x61, 0x62, 0xcd, 0x58, 0x49, 0x30, 0x59, 0x47,
                            0x36, 0x16, 0x35, 0xb6, 0xe7, 0x7d, 0x7c, 0x7a, 0x83, 0xde, 0x38, 0xc0,
                            0x80, 0x74, 0xb8, 0xc9, 0x8f, 0xd4, 0x0a, 0x43])
     for faulted_key, result in generate_faulted_results(untouched_key):
-        if result.hex() in results_sim:
+        if result in results_sim:
             if faulted_key in seen_effective_keys:
                 continue
-            seen_effective_keys[faulted_key] = results_sim[result.hex()]
+            seen_effective_keys[faulted_key] = results_sim[result]
 
     # Order by how many bits are removed from the untouched key
     # TODO: The sort does not work correctly with the shifted keys
@@ -223,33 +194,33 @@ def check_key_shortening(output_dir: str):
     ):
         print(f"Faulted key - {faulted_key.hex()}.")
         for address, hits in addresses.items():
-            print(f"Address {address} on hits {', '.join(map(str, sorted(hits)))}")
+            print(f"Address {address.hex()} on hits {', '.join(map(str, sorted(hits)))}")
         print()
 
 
 def check_known_outputs(output_dir: str):
     known_outputs_path = os.path.join(EXECUTABLE_DIR, "known_outputs.txt")
     with open(known_outputs_path, "r") as f:
-        known_outputs = f.read().splitlines()
+        known_outputs = [bytes.fromhex(line) for line in f.read().splitlines()]
     known_outputs = set(known_outputs)
-    seen_known_outputs: dict[str, dict[str, set[int]]] = {}
+    seen_known_outputs: dict[bytes, dict[bytes, set[int]]] = {}
     # TODO: parse the output only once when checking predictable outputs
     # - you are also parsing the output in check_key_shortening
     for result_sim in parse_output(output_dir):
         output = result_sim.output
         if output in known_outputs:
             if output not in seen_known_outputs:
-                seen_known_outputs[output] = {result_sim.address: set([result_sim.hit])}
+                seen_known_outputs[output] = {result_sim.executed_instruction.address: set([result_sim.executed_instruction.hit])}
             else:
-                if result_sim.address in seen_known_outputs[output]:
-                    seen_known_outputs[output][result_sim.address].add(result_sim.hit)
+                if result_sim.executed_instruction.address in seen_known_outputs[output]:
+                    seen_known_outputs[output][result_sim.executed_instruction.address].add(result_sim.executed_instruction.hit)
                 else:
-                    seen_known_outputs[output][result_sim.address] = set([result_sim.hit])
+                    seen_known_outputs[output][result_sim.executed_instruction.address] = set([result_sim.executed_instruction.hit])
 
     for output, addresses in seen_known_outputs.items():
-        print(f"Known output discovered - {output}")
+        print(f"Known output discovered - {output.hex()}")
         for address, hits in addresses.items():
-            print(f"Address {address} on hits {', '.join(map(str, sorted(hits)))}")
+            print(f"Address {address.hex()} on hits {', '.join(map(str, sorted(hits)))}")
 
 
 def check_predictable_outputs(output_dir: str):
@@ -266,9 +237,9 @@ def check_safe_error(output_dir_1: str, output_dir_2: str):
     results_sim_1_ordered: list[SimulationResult | None] = [None for _ in range(1_000_000)]
     results_sim_2_ordered: list[SimulationResult | None] = [None for _ in range(1_000_000)]
     for result_sim_1_tmp in results_sim_1:
-        results_sim_1_ordered[result_sim_1_tmp.instruction] = result_sim_1_tmp
+        results_sim_1_ordered[result_sim_1_tmp.executed_instruction.instruction] = result_sim_1_tmp
     for result_sim_2_tmp in results_sim_2:
-        results_sim_2_ordered[result_sim_2_tmp.instruction] = result_sim_2_tmp
+        results_sim_2_ordered[result_sim_2_tmp.executed_instruction.instruction] = result_sim_2_tmp
     # # All zeroes
     # correct_result_1 = get_public_key_bytes_from_private_bytes(int(0).to_bytes(32, 'big'))
     # # All ones
@@ -278,41 +249,28 @@ def check_safe_error(output_dir_1: str, output_dir_2: str):
     # All 0x6c
     correct_result_2 = get_public_key_bytes_from_private_bytes(bytes([0x6c] * 32))
 
-    potentially_prone_addresses: dict[str, set[int]] = {}
+    potentially_prone_addresses: dict[bytes, set[int]] = {}
     for result_sim_1, result_sim_2 in zip(
             results_sim_1_ordered, results_sim_2_ordered):
         if result_sim_1 is None or result_sim_2 is None:
             continue
-        assert result_sim_1.address == result_sim_2.address
-        assert result_sim_1.hit == result_sim_2.hit
-        assert result_sim_1.instruction == result_sim_2.instruction
+        assert result_sim_1.executed_instruction.address == result_sim_2.executed_instruction.address
+        assert result_sim_1.executed_instruction.hit == result_sim_2.executed_instruction.hit
+        assert result_sim_1.executed_instruction.instruction == result_sim_2.executed_instruction.instruction
 
-        if (result_sim_1.output == correct_result_1.hex()) ^ (result_sim_2.output == correct_result_2.hex()):
-            if result_sim_1.address not in potentially_prone_addresses:
-                potentially_prone_addresses[result_sim_1.address] = set()
-            potentially_prone_addresses[result_sim_1.address].add(result_sim_1.hit)
+        if (result_sim_1.output == correct_result_1) ^ (result_sim_2.output == correct_result_2):
+            if result_sim_1.executed_instruction.address not in potentially_prone_addresses:
+                potentially_prone_addresses[result_sim_1.executed_instruction.address] = set()
+            potentially_prone_addresses[result_sim_1.executed_instruction.address].add(result_sim_1.executed_instruction.hit)
 
     print("Addresses potentially prone to safe error attack:")
     for address, hits in sorted(potentially_prone_addresses.items()):
-        print(f"{address} on hits ({', '.join(map(str, sorted(hits)))})")
+        print(f"{address.hex()} on hits ({', '.join(map(str, sorted(hits)))})")
 
 
 def main():
-    # untouched_key = bytes([0x80, 0x65, 0x74, 0xba, 0x61, 0x62, 0xcd, 0x58, 0x49, 0x30, 0x59, 0x47,
-    #                        0x36, 0x16, 0x35, 0xb6, 0xe7, 0x7d, 0x7c, 0x7a, 0x83, 0xde, 0x38, 0xc0,
-    #                        0x80, 0x74, 0xb8, 0xc9, 0x8f, 0xd4, 0x0a, 0x43])
-    # for key in generate_faulted_results(untouched_key):
-    #     pass
     results_dir = os.path.join(EXECUTABLE_DIR, "sca25519-static", "outputs", "original-skips-only")
     check_predictable_outputs(results_dir)
-
-    # private_bytes = bytes.fromhex("0000000000000000000000000000000000000000000000000000000000000000")
-    # private_bytes = bytes.fromhex("806574ba6162cd5849305947361635b600000000000000000000000000000000")
-    # public_bytes = get_public_key_bytes_from_private_bytes(private_bytes)
-    # print(f"{public_bytes.hex()}")
-    # private_bytes = bytes.fromhex("856574ba6162cd5849305947361635b6e77d7c7a83de38c08074b8c98fd40ac3")
-    # public_bytes = get_public_key_bytes_from_private_bytes(private_bytes)
-    # print(f"{public_bytes.hex()}")
 
     # output_dir_1 = os.path.join(executable_dir, "sca25519-unprotected", "outputs-93")
     # output_dir_2 = os.path.join(executable_dir, "sca25519-unprotected", "outputs-6c")
