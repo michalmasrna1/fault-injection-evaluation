712f6cb780d08e2635fd206ffb43429368c27a55
Michal Masrna <m.masrna1@gmail.com>
2025-08-06 07:50:15 +0200

    process_output.py refactored

--- process_output.py
+++ process_output.py
@@ -16,78 +16,87 @@ def find_in_entry(entry: str, pattern: str, can_fail: bool = False) -> str:
     return match.group(1)
 
 
+def fault_from_entry(entry: str) -> Fault:
+    # Look for the fields Faulting Target: and Operation:,
+    # based on that, extract the relevant fields.
+    target_str = find_in_entry(entry, r'Faulting Target: (.+?)\.')
+
+    if target_str == "InstructionPointer":
+        fault_type = FaultType.SKIP
+        target = FaultTarget.PC
+        old_value = find_in_entry(entry, r'Original IP\s*?:\s*?0x([a-f0-9]+?)\s')
+        new_value = find_in_entry(entry, r'Updated IP\s*?:\s*?0x([a-f0-9]+?)\s')
+    elif target_str == "Instruction":
+        fault_type = FaultType.FLIP
+        target = FaultTarget.IR
+        old_value = find_in_entry(entry, r'Original instruction\s*?:\s*?(([a-f0-9]{2} ){2,4})\s')
+        new_value = find_in_entry(entry, r'Updated instruction\s*?:\s*?(([a-f0-9]{2} ){2,4})\s')
+    elif target_str == "Register":
+        fault_type = FaultType.ZERO
+        register_number = find_in_entry(entry, r'Reg#: (.+?)\.')
+        target = eval(f"FaultTarget.{register_number}")  # pylint: disable=W0123 (eval-used)
+        old_value = find_in_entry(entry, r'Original register\s*?:\s*?0x([a-f0-9]+?)\s')
+        new_value = find_in_entry(entry, r'Updated\s*?:\s*?0x([a-f0-9]+?)\s')
+    else:
+        raise ValueError(f"Invalid entry, unknown target: {target_str}")
+    old_value = bytes.fromhex(f"{old_value.replace(' ', ''):0>8}")
+    new_value = bytes.fromhex(f"{new_value.replace(' ', ''):0>8}")
+
+    return Fault(
+        fault_type=fault_type,
+        target=target,
+        old_value=old_value,
+        new_value=new_value
+    )
+
+def executed_instruction_from_entry(entry: str) -> ExecutedInstruction:
+    address = find_in_entry(entry, r'Address: 0x([a-f0-9]+?)\.')
+    hit = find_in_entry(entry, r'Hit: (\d+).')
+    instruction = find_in_entry(entry, r'Instruction: (\d+).')
+
+    return ExecutedInstruction(
+        address=bytes.fromhex(f"{address.strip():0>8}"),
+        hit=int(hit),
+        instruction=int(instruction)
+    )
+
+def simulation_result_from_entry(entry: str) -> SimulationResult:
+    fault = fault_from_entry(entry)
+    executed_instruction = executed_instruction_from_entry(entry)
+
+    # errored = find_in_entry(entry, r'(Errored:)', can_fail=True) != ""
+    errored = find_in_entry(entry, r'(Errored:)', can_fail=True) != "" or \
+        find_in_entry(entry, r'(Run result: fault errored program)', can_fail=True) != ""
+
+    # If the execution errored there might be no output
+    output_str = find_in_entry(entry, r'Output.+?: ([a-f0-9]+?)\s', can_fail=True)
+    if output_str:
+        output = bytes.fromhex(output_str.strip())
+    else:
+        output = NO_OUTPUT
+
+    return SimulationResult(
+        executed_instruction=executed_instruction,
+        fault=fault,
+        errored=errored,
+        output=output
+    )
+
+
 def process_output(original_path: str, destination_path: str, clean: bool) -> None:
     with open(original_path, encoding="utf-8") as original_file:
         output = original_file.read()
-    destination_file = open(destination_path, "wb")
 
-    entries = output.split("#####")
-    for entry in entries:
-        if not entry.strip():
-            # Skip empty entries (the initial new lines)
-            continue
-        # Look for the fields Faulting Target: and Operation:,
-        # based on that, extract the relevant fields.
-        target_str = find_in_entry(entry, r'Faulting Target: (.+?)\.')
+    with open(destination_path, "wb", encoding="utf-8") as destination_file:
+        entries = output.split("#####")
+        for entry in entries:
+            if not entry.strip():
+                # Skip empty entries (the initial new lines)
+                continue
 
-        if target_str == "InstructionPointer":
-            fault_type = FaultType.SKIP
-            target = FaultTarget.PC
-            old_value = find_in_entry(entry, r'Original IP\s*?:\s*?0x([a-f0-9]+?)\s')
-            new_value = find_in_entry(entry, r'Updated IP\s*?:\s*?0x([a-f0-9]+?)\s')
-        elif target_str == "Instruction":
-            fault_type = FaultType.FLIP
-            target = FaultTarget.IR
-            old_value = find_in_entry(entry, r'Original instruction\s*?:\s*?(([a-f0-9]{2} ){2,4})\s')
-            new_value = find_in_entry(entry, r'Updated instruction\s*?:\s*?(([a-f0-9]{2} ){2,4})\s')
-        elif target_str == "Register":
-            fault_type = FaultType.ZERO
-            register_number = find_in_entry(entry, r'Reg#: (.+?)\.')
-            target = eval(f"FaultTarget.{register_number}")  # pylint: disable=W0123 (eval-used)
-            old_value = find_in_entry(entry, r'Original register\s*?:\s*?0x([a-f0-9]+?)\s')
-            new_value = find_in_entry(entry, r'Updated\s*?:\s*?0x([a-f0-9]+?)\s')
-        else:
-            raise ValueError(f"Invalid entry, unknown target: {target_str}")
-        old_value = bytes.fromhex(f"{old_value.replace(' ', ''):0>8}")
-        new_value = bytes.fromhex(f"{new_value.replace(' ', ''):0>8}")
-        fault = Fault(
-            fault_type=fault_type,
-            target=target,
-            old_value=old_value,
-            new_value=new_value
-        )
+            result = simulation_result_from_entry(entry)
 
-        address = find_in_entry(entry, r'Address: 0x([a-f0-9]+?)\.')
-        hit = find_in_entry(entry, r'Hit: (\d+).')
-        instruction = find_in_entry(entry, r'Instruction: (\d+).')
-
-        # errored = find_in_entry(entry, r'(Errored:)', can_fail=True) != ""
-        errored = find_in_entry(entry, r'(Errored:)', can_fail=True) != "" or \
-            find_in_entry(entry, r'(Run result: fault errored program)', can_fail=True) != ""
-
-        # If the execution errored there might be no output
-        output_str = find_in_entry(entry, r'Output.+?: ([a-f0-9]+?)\s', can_fail=True)
-        if output_str:
-            output = bytes.fromhex(output_str.strip())
-        else:
-            output = NO_OUTPUT
-
-
-        executed_instruction = ExecutedInstruction(
-            address=bytes.fromhex(f"{address.strip():0>8}"),
-            hit=int(hit),
-            instruction=int(instruction)
-        )
-
-        result = SimulationResult(
-            executed_instruction=executed_instruction,
-            fault=fault,
-            errored=errored,
-            output=output
-        )
-        destination_file.write(result.to_bytes())
-
-    destination_file.close()
+            destination_file.write(result.to_bytes())
 
     if clean:
         # Moved here so that the file is not deleted
@@ -96,6 +105,10 @@ def process_output(original_path: str, destination_path: str, clean: bool) -> No
 
 
 def process_outputs(output_dir: str, clean: bool = False) -> None:
+    """
+    Convert all output files in the given directory from text to binary.
+    The processing is executed in parallel.
+    """
     processes: list[Process] = []
 
     for file_name in os.listdir(output_dir):
