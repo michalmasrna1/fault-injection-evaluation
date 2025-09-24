b5f573138ddf7342ce333826ae77021d5b352f02
Michal Masrna <m.masrna1@gmail.com>
2025-09-24 07:34:40 +0200

    moved safe_error_model_from_name to __init__.py for consistency

--- fi_evaluation/safe_error/__init__.py
+++ fi_evaluation/safe_error/__init__.py
@@ -1,1 +1,9 @@
-from leakage import SafeErrorModel, safe_error_model_from_name
+from leakage import KeyBits, NeighbouringBitsXor, SafeErrorModel
+
+
+def safe_error_model_from_name(name: str) -> SafeErrorModel:
+    if name == "key-bits":
+        return KeyBits()
+    if name == "neighbouring-bits-xor":
+        return NeighbouringBitsXor()
+    raise ValueError(f"Unknown safe error model: {name}. Known models are:\n- key-bits\n- neighbouring-bits-xor")
--- fi_evaluation/safe_error/leakage.py
+++ fi_evaluation/safe_error/leakage.py
@@ -44,11 +44,3 @@ class NeighbouringBitsXor(SafeErrorModel):
             previous_bit_new = current_bit_new
 
         return new_key_int.to_bytes(len(original_key), "little")
-
-
-def safe_error_model_from_name(name: str) -> SafeErrorModel:
-    if name == "key-bits":
-        return KeyBits()
-    if name == "neighbouring-bits-xor":
-        return NeighbouringBitsXor()
-    raise ValueError(f"Unknown safe error model: {name}. Known models are:\n- key-bits\n- neighbouring-bits-xor")
